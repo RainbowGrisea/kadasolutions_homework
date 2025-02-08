8829b7f672241ce5c393d97ffa3f4fdb3f51ebab
Mark Kovacs <kovmark95@gmail.com>
2025-02-08 17:52:45 +0100

    add tailwind styling and routing to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from './Components/Products';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Products />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
